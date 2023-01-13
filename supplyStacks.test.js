92b0f4b8e3cc278f103b362b6bf56f2b46c718c8
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-01-13 13:48:25 +0000

    red test

--- supplyStacks.test.js
+++ supplyStacks.test.js
@@ -1,3 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('hello');
+describe('supply Stacks part 1', () => {
+    let testData = new Map([
+        [1, ['Z', 'N']],
+        [2, ['M', 'C', 'D']],
+        [3, ['P']],
+    ]);
+    const testSupply = new Supply();
+    it('should return list of stacks', () => {
+        expect(testSupply.stacks).toEqual(testData);
+    });
+});
--- supplyStacks.test.ts
+++ supplyStacks.test.ts
@@ -1,3 +1,13 @@
 import { readFileSync } from "fs";
 
-console.log('hello')
+describe('supply Stacks part 1', () => {
+  let testData = new Map<number,string[]>([
+    [1,['Z','N']],
+    [2,['M','C','D']],
+    [3,['P']],
+  ])
+  const testSupply = new Supply()
+  it('should return list of stacks', () => {
+    expect(testSupply.stacks).toEqual(testData)
+  });
+});
