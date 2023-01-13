6bbb797b635c46d70dca81181d5ee82d77573ef3
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-01-13 15:33:52 +0000

    green 2nd test

--- supplyStacks.js
+++ supplyStacks.js
@@ -6,6 +6,21 @@ class Supply {
         this.stacks = stacks;
     }
     operateCrane(move, from, to) {
+        const stackMovingFrom = this.stacks.get(from);
+        const stackMovingTo = this.stacks.get(to);
+        const movedCrates = stackMovingFrom.splice(stackMovingFrom.length - 1, move);
+        stackMovingTo === null || stackMovingTo === void 0 ? void 0 : stackMovingTo.push(...movedCrates);
+        this.stacks.set(from, stackMovingFrom);
+        this.stacks.set(to, stackMovingTo);
     }
 }
 exports.Supply = Supply;
+let testData = new Map([
+    [1, ['Z', 'N']],
+    [2, ['M', 'C', 'D']],
+    [3, ['P']],
+]);
+const testSupply = new Supply(testData);
+console.log(testSupply.stacks);
+testSupply.operateCrane(1, 2, 1);
+console.log(testSupply.stacks);
--- supplyStacks.ts
+++ supplyStacks.ts
@@ -7,6 +7,21 @@ export class Supply{
   }
 
   operateCrane(move:number, from:number, to:number){
-    
+    const stackMovingFrom = this.stacks.get(from)!
+    const stackMovingTo = this.stacks.get(to)!
+    const movedCrates = stackMovingFrom.splice(stackMovingFrom.length - 1, move)
+    stackMovingTo?.push(...movedCrates)
+    this.stacks.set(from, stackMovingFrom)
+    this.stacks.set(to, stackMovingTo)
   }
 }
+
+let testData = new Map<number,string[]>([
+  [1,['Z','N']],
+  [2,['M','C','D']],
+  [3,['P']],
+])
+const testSupply = new Supply(testData)
+console.log(testSupply.stacks)
+testSupply.operateCrane(1,2,1)
+console.log(testSupply.stacks)
